97ace544eefeb4eaa51617cf5ea163ec04fde3d5
rajendrabairy <rajendrabairy@gmail.com>
2026-01-17 16:07:32 +0530

    Add JPA repository to fetch premium access rules

--- src/main/java/com/example/premium_access_service/repository/PremiumAccessRuleRepository.java
+++ src/main/java/com/example/premium_access_service/repository/PremiumAccessRuleRepository.java
@@ -1,4 +1,16 @@
 package com.example.premium_access_service.repository;
+import com.example.premium_access_service.domain.PremiumAccessRule;
+import com.example.premium_access_service.domain.UserLevel;
+import com.example.premium_access_service.domain.Environment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PremiumAccessRuleRepository {
-}
+
+import java.util.Optional;
+
+@Repository
+public interface PremiumAccessRuleRepository extends JpaRepository<PremiumAccessRule, Long> {
+
+    Optional <PremiumAccessRule> findByTenantIdAndEnvironmentAndUserLevel(String tenantId, Environment environment, UserLevel userLevel);
+
+};
