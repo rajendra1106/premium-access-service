4e6221efa7dca32f70e9a98d07d68bf628106b35
rajendrabairy <rajendrabairy@gmail.com>
2026-01-17 13:12:46 +0530

    Defined request and response DTOs for premium access API

--- src/main/java/com/example/premium_access_service/dto/PremiumAccessRequest.java
+++ src/main/java/com/example/premium_access_service/dto/PremiumAccessRequest.java
@@ -1,4 +1,18 @@
 package com.example.premium_access_service.dto;
+import com.example.premium_access_service.domain.Environment;
+import com.example.premium_access_service.domain.UserLevel;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class PremiumAccessRequest {
+
+    @NotNull
+    private Environment environment;
+    @NotNull
+    private UserLevel userLevel;
 }
--- src/main/java/com/example/premium_access_service/dto/PremiumAccessResponse.java
+++ src/main/java/com/example/premium_access_service/dto/PremiumAccessResponse.java
@@ -1,4 +1,10 @@
 package com.example.premium_access_service.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class PremiumAccessResponse {
+
+    private boolean premiumAccess;
 }
